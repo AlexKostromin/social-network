06dcd9be304b80751a7afdb78d4445fa4f0dec7a
Alexandr Kostromin <alexkostromin9797@gmail.com>
2024-04-04 09:48:49 +0300

    social-network

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Vanamo_Logo.png"/>
+        </header>
+    );
+};
+
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                Profile
+            </div>
+            <div>
+                Messages
+            </div>
+            <div>
+                News
+            </div>
+            <div>
+                Music
+            </div>
+            <div>
+                Setting
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
